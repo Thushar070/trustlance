499fdc469f4294ef3b002ce456a854478fdb752a
Thushar TL <thusharyyy@gmail.com>
2026-07-12 14:18:49 +0530

    network issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['10.200.48.236', '10.200.243.236'],
 };
 
 export default nextConfig;
